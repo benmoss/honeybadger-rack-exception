b5aecbdb2afaa8e245f96e12d430a10323b98904
Ben Moss <ben@movableink.com>
2014-03-24 12:07:19 -0400

    Plain 3.2.16 rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoneybadgerSwallowsExceptions::Application.config.secret_token = '613564a103ab64875544bea3c559c8d12d40fe3dd761191ec9819db02a389845c895c6016a2049a82296a3145db868305dd1e578770a8c6761b30f669cf71540'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HoneybadgerSwallowsExceptions::Application.config.session_store :cookie_store, key: '_honeybadger-swallows-exceptions_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HoneybadgerSwallowsExceptions::Application.config.session_store :active_record_store
